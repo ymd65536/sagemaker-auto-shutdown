a5c1f2d1a66728c2a01616d45e89aab8cdd58b7f
ymd65536 <ymd65536@gmail.com>
2022-12-02 05:40:13 +0900

    ダブルクォーテーションを修正

--- on-jupyter-server-start.sh
+++ on-jupyter-server-start.sh
@@ -16,7 +16,7 @@ export TIMEOUT_IN_MINS=180
 # Should already be running in user home directory, but just to check:
 cd /home/sagemaker-user
 
-# By working in a directory starting with “.”, we won’t clutter up users’ Jupyter file tree views
+# By working in a directory starting with ".”, we won’t clutter up users’ Jupyter file tree views
 mkdir -p .auto-shutdown
 
 # Create the command-line script for setting the idle timeout
@@ -27,25 +27,25 @@ import requests
 TIMEOUT=${TIMEOUT_IN_MINS}
 session = requests.Session()
 # Getting the xsrf token first from Jupyter Server
-response = session.get(“http://localhost:8888/jupyter/default/tree”)
+response = session.get("http://localhost:8888/jupyter/default/tree")
 # calls the idle_checker extension’s interface to set the timeout value
-response = session.post(“http://localhost:8888/jupyter/default/sagemaker-studio-autoshutdown/idle_checker”,
-            json={“idle_time”: TIMEOUT, “keep_terminals”: False},
-            params={“_xsrf”: response.headers[‘Set-Cookie’].split(“;”)[0].split(“=”)[1]})
+response = session.post("http://localhost:8888/jupyter/default/sagemaker-studio-autoshutdown/idle_checker",
+            json={"idle_time": TIMEOUT, "keep_terminals": False},
+            params={"_xsrf": response.headers[‘Set-Cookie’].split(";")[0].split("=")[1]})
 if response.status_code == 200:
-    print(“Succeeded, idle timeout set to {} minutes”.format(TIMEOUT))
+    print("Succeeded, idle timeout set to {} minutes".format(TIMEOUT))
 else:
-    print(“Error!“)
+    print("Error!")
     print(response.status_code)
 EOF
 chmod +x .auto-shutdown/set-time-interval.sh
 
-# “wget” is not part of the base Jupyter Server image, you need to install it first if needed to download the tarball
+# "wget” is not part of the base Jupyter Server image, you need to install it first if needed to download the tarball
 sudo yum install -y wget
 # You can download the tarball from GitHub or alternatively, if you’re using VPCOnly mode, you can host on S3
 wget -O .auto-shutdown/extension.tar.gz https://github.com/aws-samples/sagemaker-studio-auto-shutdown-extension/raw/main/sagemaker_studio_autoshutdown-0.1.5.tar.gz
 
-# Or instead, could serve the tarball from an S3 bucket in which case “wget” would not be needed:
+# Or instead, could serve the tarball from an S3 bucket in which case "wget” would not be needed:
 # aws s3 --endpoint-url [S3 Interface Endpoint] cp s3://[tarball location] .auto-shutdown/extension.tar.gz
 
 # Installs the extension
@@ -54,9 +54,9 @@ tar xzf extension.tar.gz
 cd sagemaker_studio_autoshutdown-0.1.5
 
 # Activate studio environment just for installing extension
-export AWS_SAGEMAKER_JUPYTERSERVER_IMAGE=“${AWS_SAGEMAKER_JUPYTERSERVER_IMAGE:-‘jupyter-server’}”
-if [ “$AWS_SAGEMAKER_JUPYTERSERVER_IMAGE” = “jupyter-server-3” ] ; then
-    eval “$(conda shell.bash hook)”
+export AWS_SAGEMAKER_JUPYTERSERVER_IMAGE="${AWS_SAGEMAKER_JUPYTERSERVER_IMAGE:-‘jupyter-server’}”
+if [ "$AWS_SAGEMAKER_JUPYTERSERVER_IMAGE" = "jupyter-server-3” ] ; then
+    eval "$(conda shell.bash hook)"
     conda activate studio
 fi;
 pip install --no-dependencies --no-build-isolation -e .
